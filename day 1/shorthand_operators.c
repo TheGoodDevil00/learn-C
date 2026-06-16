d89eaa9a7cb91d1e38c48ca1fb0733cc41d598c2
TheGoodDevil00 <hitesh.200711@gmail.com>
2026-06-17 00:08:29 +0530

    added shorthand operator example

---
+++ day 1/shorthand_operators.c
@@ -0,0 +1,8 @@
+int main(void){
+    int i;
+    i += 3; // same as i = i + 3
+    i -= 5; // same as i = i - 5
+    i *= 6; // same as i = i * 6
+    i /= 4; // same as i = i / 4
+    i %= 9; // same as i = i % 9
+}
