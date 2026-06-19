ece4fdc4c826378d27208963b584fb032f2eb793
TheGoodDevil00 <hitesh.200711@gmail.com>
2026-06-19 09:06:06 +0530

    added ternary operators example

---
+++ day 2/ternary_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int x,y;
+    y+=x>10? 17:37; //if x>10, then add 17 to y otherwise add 37 to y
+    //equivalent non-expression
+    if (x>10)
+        y+=17;
+    else
+        y+=37;
+}
