ed1d01edea6ea7b8e1d85f5e39a5467662f43938
TheGoodDevil00 <hitesh.200711@gmail.com>
2026-06-23 21:36:04 +0530

    sync save state

---
+++ day 3/functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*functions can accept a variety of arguments and return a value.
+The arguments and return values are predeclared*/
+
+int plus_one(int n) //this is the definition of the plus one function
+{
+    return n+1;
+}
+/*the int before the plus_one indicates the return type
+int n indicates that this function takes one int argument
+stored in parameter n, A parameter is a special type of local
+variable in which arguments are copied*/
